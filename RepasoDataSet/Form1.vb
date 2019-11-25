9c7bfb981b8aa23670e76168e9be4b6831a134e5
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-25 13:02:55 +0100

    Comienzo

---
+++ RepasoDataSet/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub NuevoPedidoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NuevoPedidoToolStripMenuItem.Click
+        Dim resp As MsgBoxResult = MsgBox("Kierres hunho nuébo?", vbYesNo)
+
+        If resp = MsgBoxResult.Yes Then
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        daPedidos.Fill(ds, "Pedidos")
+        daProductos.Fill(ds, "Productos")
+        daLineasDetalle.Fill(ds, "LineasDetalle")
+
+        Dim tab As DataTable = ds.Tables("Pedidos")
+        Dim key(0) As DataColumn
+
+        key(0) = tab.Columns("IdPedido")
+        tab.PrimaryKey = key
+
+        tab = ds.Tables("Productos")
+        key(0) = tab.Columns("IdProducto")
+        tab.PrimaryKey = key
+
+        tab = ds.Tables("LineasDetalle")
+        key(0) = tab.Columns("IdDetalle")
+        tab.PrimaryKey = key
+
+
+
+    End Sub
+End Class
---
+++ RepasoDataSet/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+
+Module Module1
+    Public cadena As String = "Data Source=SEGUNDO150;Initial Catalog=DAM_Andermaestre_DEV;Integrated Security=True"
+
+    Public daPedidos As New SqlDataAdapter("Select * From RepasoDataSet.Pedidos", cadena)
+    Public daProductos As New SqlDataAdapter("Select * From RepasoDataSet.Productos", cadena)
+    Public daLineasDetalle As New SqlDataAdapter("Select * From RepasoDataSet.LineasDetalle", cadena)
+
+    Public BuilderPedidos As New SqlCommandBuilder(daPedidos)
+    Public BuilderProductos As New SqlCommandBuilder(daProductos)
+    Public BuilderLineasDetalle As New SqlCommandBuilder(daLineasDetalle)
+
+    Public ds As New DataSet
+
+    Public Sub InicializarDataAdapter()
+        daPedidos.UpdateCommand = BuilderPedidos.GetUpdateCommand()
+        daPedidos.InsertCommand = BuilderPedidos.GetUpdateCommand()
+        daPedidos.DeleteCommand = BuilderPedidos.GetUpdateCommand()
+
+        daProductos.UpdateCommand = BuilderProductos.GetUpdateCommand()
+        daProductos.InsertCommand = BuilderProductos.GetUpdateCommand()
+        daProductos.DeleteCommand = BuilderProductos.GetUpdateCommand()
+
+        daLineasDetalle.UpdateCommand = BuilderLineasDetalle.GetUpdateCommand()
+        daLineasDetalle.InsertCommand = BuilderLineasDetalle.GetUpdateCommand()
+        daLineasDetalle.DeleteCommand = BuilderLineasDetalle.GetUpdateCommand()
+
+    End Sub
+
+End Module
